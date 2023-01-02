ff0a17aa2a31f2ee136850fe0fca515f6ead2987
Shinodegu <118846679+shinodegu@users.noreply.github.com>
2023-01-03 00:14:01 +0200

    Add files via upload

---
+++ If.py
@@ -0,0 +1,23 @@
+
+while True:
+    name = (input('Enter your name: '))
+    age = (input('Enter your age: '))
+
+    if not age.isdigit() or int(age) <= 0:
+     print('Ошибка, повторите ввод')
+     continue
+
+    elif int(age) >0 and int(age) <10:
+     print('Привет, шкет ' + name)
+
+    elif int(age) >10 and int(age) <=18 :
+     print('Как жизнь ' + name)
+
+    elif int(age) >18 and int(age) <100:
+     print('Что хотите ' + name)
+
+    else: print(name + ' Вы лжете - в наше время столько не живут...')
+
+    gameover = input('Желаете выйти (У/Д)')
+    if gameover.upper() in ('У', 'Д'):
+         break
